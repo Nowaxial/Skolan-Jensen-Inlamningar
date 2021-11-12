011a74da1b6955b09395f749efe183f8e3f557db
Nowaxial <90767515+Nowaxial@users.noreply.github.com>
2021-11-12 16:15:01 +0100

    Nästa projekt upp!
    Inlämning 2

---
+++ Javascript/Inlämning 2/javascript.js
@@ -0,0 +1,41 @@
+/*Här skriver ni koden*/
+
+var addBtn = document.getElementById("my-button");
+var getList = document.getElementById("list")
+
+console.log (addBtn, getList)
+
+//--------------------------------------------------
+addBtn.addEventListener("click", function() {
+    
+    var inputValue = document.getElementById("task").value
+    var makeLi = document.createElement("li");
+    var textInput = document.createTextNode(inputValue);
+
+    makeLi.appendChild(textInput);
+
+    if (inputValue === "") {
+      alert("Glöm inte att lägga till något på din lista!");
+    }
+     else 
+    {
+      getList.appendChild(makeLi);
+      document.getElementById("task").value = "";
+      
+      console.log(makeLi)
+    }
+    var deletebtn = document.createElement("button");
+    var txt = document.createTextNode("\u00D7");
+    deletebtn.className = "delete";
+    //deletebtn.setAttribute("class","delete");
+    deletebtn.appendChild(txt);
+    makeLi.appendChild(deletebtn); 
+    
+
+    deletebtn.addEventListener("click", () => {
+    var list = document.getElementById("list");  
+    list.removeChild(list.childNodes[0]);      // Remove <ul>'s first child node (index 0)
+    });
+
+
+});
